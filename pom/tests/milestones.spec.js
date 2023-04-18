a5a227e25f7c71a30d558ff6655b9d0dce155b53
Ruben <ruben.mte@gmail.com>
2023-04-18 19:07:00 +0200

    commit

--- pom/tests/milestones.spec.js
+++ pom/tests/milestones.spec.js
@@ -2,20 +2,24 @@ import { test } from '@playwright/test';
 import { URLS, CREDENTIALS } from '../data/Constants';
 const { LoginPage } = require('../pages/loginPage');
 
+/*
 test('Create a milestone', async ({ page }) => {
     
     const loginPage = new LoginPage(page);
-    
+
     await page.goto(URLS.HOME);
     await loginPage.submitLogin(CREDENTIALS.USER,CREDENTIALS.PASS);
 
     await page.getByRole('tab', { name: 'My projects' }).click();
     await page.frameLocator('iframe').getByRole('link', { name: 'Project A' }).click();
 
-    await page.getByRole('link', { name: 'Milestones' }).click();
+
+    await page.locator("//li[@class='milestones-tab']//a").click();
+    
 
     await page.frameLocator('iframe').getByRole('button', { name: 'Add Milestone' }).click();
     await page.frameLocator('iframe').getByPlaceholder('Give this milestone a name').fill('My first milestone');
     await page.frameLocator('iframe').locator('#addMilestoneForm').getByRole('button', { name: 'Add Milestone' }).click();
   
 });
+*/
--- pom/tests/tasks.spec.js
+++ pom/tests/tasks.spec.js
@@ -3,7 +3,9 @@ import { URLS, CREDENTIALS } from '../data/Constants';
 const { LoginPage } = require('../pages/loginPage');
 const { TasksPage } = require('../pages/tasksPage');
 
+
 test('Create a task', async ({ page }) => {
+
   const loginPage = new LoginPage(page);
   const tasksPage = new TasksPage(page);
   const taskListName = 'Task List 1'
@@ -13,10 +15,12 @@ test('Create a task', async ({ page }) => {
   await loginPage.submitLogin(CREDENTIALS.USER,CREDENTIALS.PASS);
   await tasksPage.newTaskList(taskListName);
   await tasksPage.newTask(taskName);
+
 });
 
 
 test('Complete a task', async ({ page }) => {
+  
   const loginPage = new LoginPage(page);
   const tasksPage = new TasksPage(page);
   const taskListName = 'Task List 2'
@@ -27,6 +31,9 @@ test('Complete a task', async ({ page }) => {
   await tasksPage.newTaskList(taskListName);
   await tasksPage.newTask(taskName);
   
+  const element = page.locator("//a[text()='Task List 2']/parent::h3/parent::div/parent::div/descendant::button[1]");
+  await element.click();
+
   //await tasksPage.completeTask();
 
 });
