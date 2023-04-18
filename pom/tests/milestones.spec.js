d99c4dcca4c243354ceb17f77b15244206b0b36e
Ruben <ruben.mte@gmail.com>
2023-04-18 14:15:28 +0200

    Create first test

--- pom/tests/milestones.spec.js
+++ pom/tests/milestones.spec.js
@@ -1,15 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { URLS, CREDENTIALS } from '../data/Constants';
 const { LoginPage } = require('../pages/loginPage');
-const { TasksPage } = require('../pages/tasksPage');
+const { MilestonesPage } = require('../pages/milestonesPage');
 
 test('Create a milestone', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  const tasksPage = new TasksPage(page);
+  const milestonesPage = new MilestonesPage(page);
   
   await page.goto(URLS.HOME);
   await loginPage.submitLogin(CREDENTIALS.USER,CREDENTIALS.PASS);
   
+  await page.getByRole('tab', { name: 'My projects' }).click();
+  await page.frameLocator('iframe').getByRole('link', { name: 'Project A' }).click();
   await page.getByRole('link', { name: 'Milestones' }).click();
   await page.frameLocator('iframe').getByRole('button', { name: 'plus icon Add the First Milestone' }).click();
   await page.frameLocator('iframe').getByPlaceholder('Give this milestone a name').fill('My milestone');
