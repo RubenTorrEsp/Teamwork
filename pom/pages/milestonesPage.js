7e1d77e87135ea8a0c93f269a958a39d91621b8d
Ruben <ruben.mte@gmail.com>
2023-04-19 13:23:15 +0200

    Milestone pom

--- pom/pages/milestonesPage.js
+++ pom/pages/milestonesPage.js
@@ -8,16 +8,29 @@ exports.MilestonesPage = class MilestonesPage {
    */
   constructor(page) {
     this.page = page;
+
+    this.projects = page.getByRole('tab', { name: 'My projects' });
+    this.projectA = page.frameLocator('iframe').getByRole('link', { name: 'Project A' });
+
+    this.moreTab = page.getByRole('tab', { name: 'More...' });
+    this.milestonesLink = page.getByRole('link', { name: 'Milestones' });
+
+    this.addMilestone = page.frameLocator('iframe').getByRole('button', { name: 'Add Milestone' });
+    this.milestoneName = page.frameLocator('iframe').getByPlaceholder('Give this milestone a name');
+    this.completeMilestone = page.frameLocator('iframe').locator('#addMilestoneForm').getByRole('button', { name: 'Add Milestone' });
   }
 
 
-  async newMilestone(){
-    await page.getByRole('tab', { name: 'My projects' }).click();
-    await page.frameLocator('iframe').getByRole('link', { name: 'Project A' }).click();
-    await page.getByRole('link', { name: 'Milestones' }).click();
-    await page.frameLocator('iframe').getByRole('button', { name: 'plus icon Add the First Milestone' }).click();
-    await page.frameLocator('iframe').getByPlaceholder('Give this milestone a name').fill('My milestone');
-    await page.frameLocator('iframe').locator('#addMilestoneForm').getByRole('button', { name: 'Add Milestone' }).click();
+  async newMilestone(name){
+    await this.projects.click();
+    await this.projectA.click();
+
+    await this.moreTab.hover();
+    await this.milestonesLink.click();
+
+    await this.addMilestone.click();
+    await this.milestoneName.fill(name);
+    await this.completeMilestone.click();
   }
 
 
--- pom/tests/milestones.spec.js
+++ pom/tests/milestones.spec.js
@@ -1,22 +1,14 @@
 import { test } from '@playwright/test';
 import { URLS, CREDENTIALS } from '../data/Constants';
 const { LoginPage } = require('../pages/loginPage');
+const { MilestonesPage } = require('../pages/milestonesPage');
 
 test('Create a milestone', async ({ page }) => {
-    
     const loginPage = new LoginPage(page);
+    const milestonesPage = new MilestonesPage(page);
+    const milestoneName = 'My first milestone';
 
     await page.goto(URLS.HOME);
     await loginPage.submitLogin(CREDENTIALS.USER,CREDENTIALS.PASS);
-
-    await page.getByRole('tab', { name: 'My projects' }).click();
-    await page.frameLocator('iframe').getByRole('link', { name: 'Project A' }).click();
-
-    await page.getByRole('tab', { name: 'More...' }).hover();
-    await page.getByRole('link', { name: 'Milestones' }).click();
-
-    await page.frameLocator('iframe').getByRole('button', { name: 'Add Milestone' }).click();
-    await page.frameLocator('iframe').getByPlaceholder('Give this milestone a name').fill('My first milestone');
-    await page.frameLocator('iframe').locator('#addMilestoneForm').getByRole('button', { name: 'Add Milestone' }).click();
-  
+    await milestonesPage.newMilestone(milestoneName);
 });
